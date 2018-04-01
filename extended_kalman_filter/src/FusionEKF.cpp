d2dbd13f345eeba1cc83559a9fc27c29f2fff11b
zuqqhi2 <zuqqhi2@gmail.com>
2018-04-01 02:55:19 +0000

    EKF fix bug

--- extended_kalman_filter/src/FusionEKF.cpp
+++ extended_kalman_filter/src/FusionEKF.cpp
@@ -38,8 +38,6 @@ FusionEKF::FusionEKF() {
   */
   H_laser_ << 1, 0, 0, 0,
               0, 1, 0, 0;
-  noise_ax = 9;
-  noise_ay = 9;
 }
 
 /**
@@ -74,7 +72,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       /**
       Initialize state.
       */
-      ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements[1], 0, 0;
+      ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
     }
 
     // done initializing, no need to predict or update
@@ -94,7 +92,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the process noise covariance matrix.
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
-  //compute the time elapsed between the current and previous measurements
+   //compute the time elapsed between the current and previous measurements
   float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
   previous_timestamp_ = measurement_pack.timestamp_;
 
@@ -102,17 +100,18 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   float dt_3 = dt_2 * dt;
   float dt_4 = dt_3 * dt;
 
-  //Modify the F matrix so that the time is integrated
+   //Modify the F matrix so that the time is integrated
   ekf_.F_(0, 2) = dt;
   ekf_.F_(1, 3) = dt;
 
-  //set the process covariance matrix Q
+   //set the process covariance matrix Q
+  float noise_ax = 9;
+  float noise_ay = 9;
   ekf_.Q_ = MatrixXd(4, 4);
   ekf_.Q_ <<  dt_4/4*noise_ax, 0,               dt_3/2*noise_ax, 0,
               0,               dt_4/4*noise_ay, 0,               dt_3/2*noise_ay,
               dt_3/2*noise_ax, 0,               dt_2*noise_ax,   0,
               0,               dt_3/2*noise_ay, 0,               dt_2*noise_ay;
-
   ekf_.Predict();
 
   /*****************************************************************************
@@ -129,6 +128,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     // Radar updates
   } else {
     // Laser updates
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
